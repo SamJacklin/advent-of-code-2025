cc806aa07e1dfad288b06d4908adaa41131ab090
Sam Jacklin <sam11jacklin@gmail.com>
2025-12-05 13:43:55 +0000

    Update day 4 part 2 solution

--- aoc_solutions/day_4/solution_p2.py
+++ aoc_solutions/day_4/solution_p2.py
@@ -1,43 +1,53 @@
 """Advent of Code - Day 4 Part 2"""
 
 from aoc_solutions.utils import get_input, submit_answer
+import queue
 
 DAY = 4
 
 DIRECTIONS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 LIMIT = 3
 
-def check_char(row: int, col: int, lines: list[str]) -> bool:
+def check_char(row: int, col: int, lines: list[str]) -> tuple[bool, list[tuple[int, int]]]:
     count = 0
+    to_recheck = []
 
     for d_row, d_col in DIRECTIONS:
         n_row, n_col = row + d_row, col + d_col
         # Check bounds
         if 0 <= n_row < len(lines) and 0 <= n_col < len(lines[n_row]):
             if lines[n_row][n_col] == '@':
+                to_recheck.append((n_row, n_col))
                 count += 1
     if count > LIMIT:
-        return False
-    return True
+        return False, None
+    return True, to_recheck
 
 def part_2(data: str) -> int:
     count = 0
-    complete = False
+    recheck_queue = queue.Queue()
     lines = [list(line) for line in data.splitlines()]
-    while not complete:
-        complete = True
-        for row, line in enumerate(lines):
-            if not complete:
-                break
-            for col, char in enumerate(line):
-                if char == '@':
-                    if check_char(row, col, lines):
-                        count += 1
-                        lines[row][col] = '.'
-                        complete = False
-                        break
+    for row, line in enumerate(lines):
+        for col, char in enumerate(line):
+            if char == '@':
+                is_removable, to_recheck = check_char(row, col, lines)
+                if is_removable:
+                    count += 1
+                    lines[row][col] = '.'
+                    for item in to_recheck:
+                        recheck_queue.put(item)
 
-        # no changes made during iteration
+    while not recheck_queue.empty():
+        row, col = recheck_queue.get()
+        if lines[row][col] != '@':
+            continue
+        is_removable, to_recheck = check_char(row, col, lines)
+        if is_removable:
+            count += 1
+            lines[row][col] = '.'
+            for item in to_recheck:
+                recheck_queue.put(item)
+
     return count
 
 
